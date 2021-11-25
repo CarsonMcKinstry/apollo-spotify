01f81068284cf8c5eacae0f318a3666b2cd8b1aa
Carson McKinstry <carson.mckinstry@trivago.com>
2021-11-25 09:10:10 +0100

    added tracks to the user mocks

--- src/schemas/user/__tests__/user.mock.ts
+++ src/schemas/user/__tests__/user.mock.ts
@@ -128,7 +128,101 @@ export const myTopTracksMock = (token: string, args: MeTopTracksArgs = {}) =>
     .get("/me/top/tracks")
     .query(args)
     .reply(200, {
-      items: [],
+      items: [
+        {
+          id: "string",
+          name: "string",
+          type: "track",
+          available_markets: ["CA"],
+          disc_number: 0,
+          track_number: 0,
+          duration_ms: 0,
+          explicit: true,
+
+          popularity: 0,
+          restrictions: {
+            reason: "string",
+          },
+
+          uri: "string",
+          preview_url: "string",
+          external_ids: {
+            isrc: "string",
+            ean: "string",
+            upc: "string",
+          },
+          external_urls: {
+            spotify: "string",
+          },
+          is_playable: true,
+
+          is_local: true,
+          album: {
+            album_type: "compilation",
+            total_tracks: 9,
+            available_markets: ["CA", "BR", "IT"],
+            external_urls: {
+              spotify: "string",
+            },
+            href: "string",
+            id: "2up3OPMp9Tb4dAKM2erWXQ",
+            images: [
+              {
+                url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\n",
+                height: 300,
+                width: 300,
+              },
+            ],
+            name: "string",
+            release_date: "1981-12",
+            release_date_precision: "year",
+            restrictions: {
+              reason: "market",
+            },
+            type: "album",
+            uri: "spotify:album:2up3OPMp9Tb4dAKM2erWXQ",
+            artists: [
+              {
+                external_urls: {
+                  spotify: "string",
+                },
+                href: "string",
+                id: "string",
+                name: "string",
+                type: "artist",
+                uri: "string",
+              },
+            ],
+          },
+          artists: [
+            {
+              external_urls: {
+                spotify: "string",
+              },
+              followers: {
+                href: "string",
+                total: 0,
+              },
+              genres: ["Prog rock", "Grunge"],
+              href: "string",
+              id: "string",
+              images: [
+                {
+                  url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\n",
+                  height: 300,
+                  width: 300,
+                },
+              ],
+              name: "string",
+              popularity: 0,
+              type: "artist",
+              uri: "string",
+            },
+          ],
+
+          href: "string",
+        },
+      ],
       limit: args.limit ?? 20,
       next: `https://api.spotify.com/v1/me/tracks?offset=${
         args.offset ?? 1
@@ -141,7 +235,101 @@ export const myTopTracksMock = (token: string, args: MeTopTracksArgs = {}) =>
     });
 
 export const myTopTracksResponseMock = (args: MeTopTracksArgs = {}) => ({
-  tracks: [],
+  tracks: [
+    {
+      id: "string",
+      name: "string",
+      type: "track",
+      availableMarkets: ["CA"],
+      discNumber: 0,
+      trackNumber: 0,
+      duration: 0,
+      explicit: true,
+
+      popularity: 0,
+      restrictions: {
+        reason: "string",
+      },
+
+      uri: "string",
+      previewUrl: "string",
+      externalIds: {
+        isrc: "string",
+        ean: "string",
+        upc: "string",
+      },
+      externalUrls: {
+        spotify: "string",
+      },
+      isPlayable: true,
+
+      isLocal: true,
+      album: {
+        albumType: "compilation",
+        totalTracks: 9,
+        availableMarkets: ["CA", "BR", "IT"],
+        externalUrls: {
+          spotify: "string",
+        },
+        href: "string",
+        id: "2up3OPMp9Tb4dAKM2erWXQ",
+        images: [
+          {
+            url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\n",
+            height: 300,
+            width: 300,
+          },
+        ],
+        name: "string",
+        releaseDate: "1981-12",
+        releaseDatePrecision: "year",
+        restrictions: {
+          reason: "market",
+        },
+        type: "album",
+        uri: "spotify:album:2up3OPMp9Tb4dAKM2erWXQ",
+        artists: [
+          {
+            externalUrls: {
+              spotify: "string",
+            },
+            href: "string",
+            id: "string",
+            name: "string",
+            type: "artist",
+            uri: "string",
+          },
+        ],
+      },
+      artists: [
+        {
+          externalUrls: {
+            spotify: "string",
+          },
+          followers: {
+            href: "string",
+            total: 0,
+          },
+          genres: ["Prog rock", "Grunge"],
+          href: "string",
+          id: "string",
+          images: [
+            {
+              url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\n",
+              height: 300,
+              width: 300,
+            },
+          ],
+          name: "string",
+          popularity: 0,
+          type: "artist",
+          uri: "string",
+        },
+      ],
+
+      href: "string",
+    },
+  ],
   limit: args.limit ?? 20,
   next: args.offset ?? 1,
   offset: args.offset ?? 0,
@@ -158,7 +346,31 @@ export const myTopArtistsMock = (token: string, args: MeTopArtistsArgs = {}) =>
     .get("/me/top/artists")
     .query(args)
     .reply(200, {
-      items: [],
+      items: [
+        {
+          external_urls: {
+            spotify: "string",
+          },
+          followers: {
+            href: "string",
+            total: 0,
+          },
+          genres: ["Prog rock", "Grunge"],
+          href: "string",
+          id: "string",
+          images: [
+            {
+              url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\n",
+              height: 300,
+              width: 300,
+            },
+          ],
+          name: "string",
+          popularity: 0,
+          type: "artist",
+          uri: "string",
+        },
+      ],
       limit: args.limit ?? 20,
       next: `https://api.spotify.com/v1/me/artists?offset=${
         args.offset ?? 1
@@ -171,7 +383,31 @@ export const myTopArtistsMock = (token: string, args: MeTopArtistsArgs = {}) =>
     });
 
 export const myTopArtistsResponseMock = (args: MeTopArtistsArgs = {}) => ({
-  artists: [],
+  artists: [
+    {
+      externalUrls: {
+        spotify: "string",
+      },
+      followers: {
+        href: "string",
+        total: 0,
+      },
+      genres: ["Prog rock", "Grunge"],
+      href: "string",
+      id: "string",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228\n",
+          height: 300,
+          width: 300,
+        },
+      ],
+      name: "string",
+      popularity: 0,
+      type: "artist",
+      uri: "string",
+    },
+  ],
   limit: args.limit ?? 20,
   next: args.offset ?? 1,
   offset: args.offset ?? 0,
